dfec6fc9a1438c10703a0a2ef969e6f4b6cbb28e
Momchil Asenov <momchil276@gmail.com>
2021-09-02 21:25:28 +0300

    Pet Clinic initial commit

---
+++ src/test/java/com/petclinic/sfgpetclinic/PetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package com.petclinic.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
