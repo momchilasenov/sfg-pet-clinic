2878298fb1bfb42087fbf44b9a32f05ae3943a28
Momchil Asenov <momchil276@gmail.com>
2021-09-09 22:22:32 +0300

    Added Map Based Services. Closes #6

---
+++ src/main/java/com/petclinic/sfgpetclinic/services/map/PersonServiceMap.java
@@ -0,0 +1,39 @@
+package com.petclinic.sfgpetclinic.services.map;
+
+import com.petclinic.sfgpetclinic.models.Person;
+import com.petclinic.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PersonServiceMap extends AbstractMapService<Person, Long> implements CrudService<Person, Long>
+{
+  @Override
+  public Set<Person> findAll()
+  {
+    return super.findAll();
+  }
+
+  @Override
+  public void deleteById(Long id)
+  {
+    super.deleteById(id);
+  }
+
+  @Override
+  public void delete(Person object)
+  {
+    super.delete(object);
+  }
+
+  @Override
+  public Person save(Person object)
+  {
+    return super.save(object.getId(), object);
+  }
+
+  @Override
+  public Person findById(Long id)
+  {
+    return super.findById(id);
+  }
+}
