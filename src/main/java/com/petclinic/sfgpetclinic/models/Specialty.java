fbe09afdcf8e864aef21d8839fc6279718659653
Momchil Asenov <momchil276@gmail.com>
2022-05-24 00:41:53 +0300

    - convert vets to JPA entities - closes #34

--- src/main/java/com/petclinic/sfgpetclinic/models/Specialty.java
+++ src/main/java/com/petclinic/sfgpetclinic/models/Specialty.java
@@ -1,5 +1,10 @@
 package com.petclinic.sfgpetclinic.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity
 {
   private String description;
